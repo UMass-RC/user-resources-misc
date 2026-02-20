6ec74fb2b63039209d72c0315196bc459be24975
Simon Leary <simon.leary42@proton.me>
2026-02-20 11:09:13 -0500

    msising period

--- unity_user_resources_misc/unity_account_expiry_warning.py
+++ unity_user_resources_misc/unity_account_expiry_warning.py
@@ -80,7 +80,7 @@ def print_pi_group_owner_disable_warning(group_data: list[tuple]):
         table = [["Group Name", "Owner Username", "Time until Disable"]] + group_data
         print("\n".join(fmt_table(table)))
         print(f"To prevent this, each group owner must simply log in to the {PORTAL}.")
-    print(f"For more information, see our {POLICY}")
+    print(f"For more information, see our {POLICY}.")
     print()
 
 
