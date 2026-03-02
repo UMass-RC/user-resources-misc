efb18cab3017ad193ff824a7beb7afab738bc176
Simon Leary <simon.leary42@proton.me>
2026-03-02 11:06:44 -0500

    expiry status script print same message as expiry warning script

--- unity_user_resources_misc/unity_account_expiry_status.py
+++ unity_user_resources_misc/unity_account_expiry_status.py
@@ -4,6 +4,7 @@ import pwd
 from unity_user_resources_misc.unity_account_expiry_warning import (
     IDLELOCK_WARNING_THRESHOLD_DAYS,
     get_expiry_data,
+    print_idlelock_warning,
     time_until,
 )
 
@@ -13,8 +14,6 @@ def main():
     data = get_expiry_data(username)
     time_until_idlelock = time_until(data["idlelock_date"])
     if time_until_idlelock.days < IDLELOCK_WARNING_THRESHOLD_DAYS:
-        print(
-            f"Your account is considered idle, and is scheduled to be locked in {time_until_idlelock.days} days."
-        )
+        print_idlelock_warning(time_until_idlelock)
     else:
         print("Your account is not considered idle, and is not scheduled to be locked.")
